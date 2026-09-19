be575cb86321d8290f34852304d046c5f5bdc2fe
Mukesh THAKURI <S2600359@naps.edu.au>
2026-09-19 14:10:59 +0545

    Add StudySession class

---
+++ StudySession.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentStudyPlanner
+{
+    public class StudySession
+    {
+        public int Id { get; set; }
+
+        public string Subject { get; set; } = "";
+
+        public DateTime Date { get; set; }
+
+        public TimeSpan StartTime { get; set; }
+
+        public int Duration { get; set; }
+
+        public string Notes { get; set; } = "";
+    }
+}
